c7d8001adfc09944d7f18574272acaf6c20009a1
Matthew Geiger <4188269+popensesame@users.noreply.github.com>
2024-08-10 16:29:23 -0400

    Remove sample component

--- src/components/SampleButton.jsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-
-export default function SampleButton({ count, onClick, message }) {
-  return <Button onClick={ onClick } variant="contained">{`You have clicked me ${count} times!`}</Button>;
-}
