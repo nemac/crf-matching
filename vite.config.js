339609951fbfb2ea4653c982005a3f94bbca5a95
Jeff Bliss <jeff.a.bliss@gmail.com>
2024-12-06 10:12:11 -0500

    comment out base for now

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
       __AIRTABLE_TOKEN__: JSON.stringify(env.AIRTABLE_TOKEN),
-      __AIRTABLE_BASE__: JSON.stringify(env.AIRTABLE_BASE)
+      __AIRTABLE_BASE__: JSON.stringify(env.AIRTABLE_BASE),
     },
-    base: 'https://nemac.github.io/crf-matching',
+    //base: 'https://nemac.github.io/crf-matching',
     plugins: [react()],
-  }
-})
+  };
+});
