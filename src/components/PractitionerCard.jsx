db7ecb1014eb518f0a38f0929c3e7d16b72f149b
Dani Levy <dlevy@unca.edu>
2024-12-09 11:21:56 -0600

    Cards mobile

--- src/components/PractitionerCard.jsx
+++ src/components/PractitionerCard.jsx
@@ -71,7 +71,7 @@ export default function PractitionerCard({ practitioner }) {
               display: 'flex',
               gap: '2px',
               flexDirection: 'row',
-              flexWrap: 'nowrap',
+              flexWrap: 'wrap',
             }}
           >
             {displayedActivities.map((activity, index) => (
@@ -88,7 +88,6 @@ export default function PractitionerCard({ practitioner }) {
                   alignItems: 'center',
                   justifyContent: 'center',
                   minHeight: '32px',
-                  flex: '1 1 0px',
                   textAlign: 'center',
                 }}
               >
