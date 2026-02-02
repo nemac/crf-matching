0c37de04fd21083ce203d22c878b8f5ac1929f5f
Anis Golriz <anisgolriz@MacBookPro.attlocal.net>
2026-02-02 14:52:37 -0500

    Updated cta for proper formatting

--- src/components/baseComponents/CallToActionButton.jsx
+++ src/components/baseComponents/CallToActionButton.jsx
@@ -1,24 +1,29 @@
 import { Button } from '@mui/material';
 import ArrowRightAltOutlinedIcon from '@mui/icons-material/ArrowRightAltOutlined';
 const CallToActionButton = () => {
-  return (
-    <>
-      <Button
-        endIcon={<ArrowRightAltOutlinedIcon />}
-        sx={{
-          height: '46px',
-          width: '132px',
-          color: '#FFFFFF',
-          backgroundColor: '#003366',
-          '&:hover': {
-            bgcolor: '#44619A',
-          },
-        }}
-      >
-        Action
-      </Button>
-    </>
-  );
+    return (
+        <>
+            <Button
+                endIcon={<ArrowRightAltOutlinedIcon />}
+                sx={{
+                    height: '46px',
+                    width: '132px',
+                    color: '#FFFFFF',
+                    pt:'4px',
+                    pl:'12px',
+                    pb:'4px',
+                    pr:'12px',
+                    gap:'16px',
+                    backgroundColor: '#003366',
+                    '&:hover': {
+                        bgcolor: '#44619A',
+                    },
+                }}
+            >
+                Action
+            </Button>
+        </>
+    );
 };
 
 export default CallToActionButton;
