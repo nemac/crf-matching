a9f23115bcabdc07bafe22545b25abd508c1d4fc
Jeff Bliss <jeff.a.bliss@gmail.com>
2026-03-02 09:58:51 -0500

    prettier and remove fixed width and height

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,41 +1,37 @@
-
 import Box from '@mui/material/Box';
 import csciLogo from '../assets/Registry_Logo_new.png';
 import theme from '../theme.jsx';
-import useMediaQuery from "@mui/material/useMediaQuery";
+import useMediaQuery from '@mui/material/useMediaQuery';
 import { Link } from 'react-router-dom';
 import { Typography } from '@mui/material';
 
-
-export default function Logo () {
-    const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
-    const logoWidth = isSmallScreen ? 56 : 86;
-    const params = window.location.search || '';
-    return (
-        <Box
-            sx={{
-                display: 'flex',
-                width: '100%',
-                alignItems: 'center',
-                justifyContent: {xs: 'center', sm: 'flex-start', md: 'flex-start'},
-                p: 2,
-                gap:1,
-            }}
-        >
-            <Link to={'/Registry' + params}>
-                <img
-                    src={csciLogo}
-                    alt="Registry of Adaptation Practitioners Logo"
-                    style={{
-                        margin: '0 auto',
-                        height: 'auto',
-                        width: `${logoWidth}px`,
-                    }}
-                />
-            </Link>
-            <Typography  width={321} height={23}>
-                Registry of Adaptation Practitioners
-            </Typography>
-        </Box>
-    )
+export default function Logo() {
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
+  const logoWidth = isSmallScreen ? 56 : 86;
+  const params = window.location.search || '';
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: { xs: 'center', sm: 'flex-start', md: 'flex-start' },
+        p: 2,
+        gap: 1,
+      }}
+    >
+      <Link to={`/Registry${params}`}>
+        <img
+          src={csciLogo}
+          alt="Registry of Adaptation Practitioners Logo"
+          style={{
+            margin: '0 auto',
+            height: 'auto',
+            width: `${logoWidth}px`,
+          }}
+        />
+      </Link>
+      <Typography>Registry of Adaptation Practitioners</Typography>
+    </Box>
+  );
 }
