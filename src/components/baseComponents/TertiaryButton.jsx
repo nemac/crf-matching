ce19a12f078174012a8e22cadb95d37b22c09742
Jeff Bliss <jeff.a.bliss@gmail.com>
2026-03-13 10:32:00 -0400

    why did this get lost

--- src/components/baseComponents/TertiaryButton.jsx
+++ src/components/baseComponents/TertiaryButton.jsx
@@ -1,23 +1,22 @@
 import { Button } from '@mui/material';
 
-const TertiaryButton = () => {
+const TertiaryButton = ({ children = 'Action', sx, ...props }) => {
   return (
-    <>
-      <Button
-        sx={{
-          width: 'auto',
-          height: '37px',
-          pt: '8px',
-          pb: '8px',
-          backgroundColor: '#FFFFFF',
-          '&:hover': {
-            bgcolor: '#99DDFF',
-          },
-        }}
-      >
-        Action
-      </Button>
-    </>
+    <Button
+      sx={{
+        width: 'auto',
+        pt: '8px',
+        pb: '8px',
+        backgroundColor: '#FFFFFF',
+        '&:hover': {
+          bgcolor: '#99DDFF',
+        },
+        ...sx,
+      }}
+      {...props}
+    >
+      {children}
+    </Button>
   );
 };
 export default TertiaryButton;
