8a30f7f67920eaafd2f26ce003b1ebbe20e235bb
Anis Golriz <anisgolriz@Aniss-MacBook-Pro.local>
2026-03-13 16:40:22 -0400

    Added the find button of the search bar

--- src/components/baseComponents/SearchBar.jsx
+++ src/components/baseComponents/SearchBar.jsx
@@ -1,5 +1,6 @@
-import TextField from '@mui/material/TextField';
 import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import { InputAdornment } from '@mui/material';
 const SearchBar = props => {
   const { text, textSx } = props;
   return (
@@ -17,12 +18,19 @@ const SearchBar = props => {
       <InputBase
         placeholder={text ?? 'to be filled'}
         inputProps={{ 'aria-label': 'search' }}
+        endAdornment=<InputAdornment posiiton="end">
+          <SearchIcon />
+        </InputAdornment>
         sx={{
           backgroundColor: '#FFFFFF',
           minWidth: 320,
-          borderRadius: 12,
-          '& input::placeholder': {
-            p: 2,
+          borderRadius: 3,
+          '& .MuiInputAdornment-root': {
+            pr: 2,
+          },
+          '& input': {
+            pl: 3,
+            pt: 0.5,
           },
         }}
       >
